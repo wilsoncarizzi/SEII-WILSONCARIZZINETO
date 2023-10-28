f10d5c969b034f48e313b2cc6d62a5db5a1507be
Wilson Carizzi Neto <wilsoncarizzi59@gmail.com>
2023-10-28 20:48:33 -0300

    Atividade SDG semana 6

---
+++ Semana06/ex04/Servidor02.py
@@ -0,0 +1,22 @@
+import socket
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientsocket.send(bytes("Welcome to the server", "utf-8"))
+    
+    while True:
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientsocket.send(bytes(msg, "utf-8"))
